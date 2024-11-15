e8b8153b38228a1070d82b2f841adfa1fba5af2f
andresito05 <guarnizoandres004@gmail.com>
2024-11-15 13:11:58 -0500

    metodos api

---
+++ js/api.js
@@ -0,0 +1,39 @@
+const API_URL = "http://localhost:3000/api/Juegosya";
+
+export const  getAllJuegosyaa = async () => { 
+    const response = await fetch(API_URL); 
+    return response.json(); 
+};   
+
+//obtener productos por ID 
+export const getJuegosyaById = async (id) => {
+    const response = await fetch(`}${API_URL}/${id}`);
+    return response.json(); 
+}; 
+
+// Crear un producto 
+export const createJuegosya = async (product) => {
+const response = await fetch(API_URL, {
+    method: "POST",
+    headers: { "content-type": "application/json"},
+    body: JSON.stringify(product) 
+});
+return response.json();
+};
+
+//Actualizar un producto 
+export const updateJuegosya  = async (id, product) => {
+    const response = await fetch(`${API_URL}/${id}`, {
+        method: "PUT",
+        headers: { "content-type": "application/json"},
+        body: JSON.stringify(product) 
+    });
+    return response.json();
+    };
+
+    //Borra un producto 
+    export const deleteJuegosya  = async (id) => {
+        return fetch(`${API_URL}/${id}`, {
+            method: "DELETE",
+        });
+        };
